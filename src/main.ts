90ca039abdb9d5eea2f66fa5d8ac393641681439
Mangala SSS Khalsa <mkhalsa@sitepen.com>
2017-02-09 20:23:20 -0700

    TitlePanel: add to main module

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import createTitlePanel from './components/titlePanel/createTitlePanel';
 import Dialog from './dialog/Dialog';
 import SlidePane from './slidepane/SlidePane';
 import Label from './label/Label';
@@ -9,6 +10,7 @@ import Radio from './radio/Radio';
 import Slider from './slider/Slider';
 
 export {
+	createTitlePanel,
 	Dialog,
 	SlidePane,
 	Label,
